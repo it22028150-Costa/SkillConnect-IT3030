2ef9b46ffc42c89f2ac9f446a1ed6b85835f0753
Sanjana de Costa <IT22028150@my.sliit.lk>
2025-04-30 16:25:15 +0530

    Initialized Backend

---
+++ SkillConnect/src/main/java/com/zos/SkillConnect.java
@@ -0,0 +1,13 @@
+package com.zos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SkillConnect {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SkillConnect.class, args);
+	}
+
+}
