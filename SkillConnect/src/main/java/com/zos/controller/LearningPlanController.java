b95ac6686df6b803dec6222e891026d42b7644e0
Sandali Maneesha <IT22562838@my.sliit.lk>
2025-05-12 12:00:00 +0530

    Test

--- SkillConnect/src/main/java/com/zos/controller/LearningPlanController.java
+++ SkillConnect/src/main/java/com/zos/controller/LearningPlanController.java
@@ -30,8 +30,7 @@ public class LearningPlanController {
     private UserService userService;
 
     @PostMapping("/")
-    public ResponseEntity<LearningPlan> createLearningPlan(@Valid
-            @RequestBody LearningPlan learningPlan,
+    public ResponseEntity<LearningPlan> createLearningPlan(@Valid @RequestBody LearningPlan learningPlan,
             @RequestHeader("Authorization") String token) throws UserException {
 
         User user = userService.findUserProfile(token);
@@ -144,3 +143,4 @@ public class LearningPlanController {
         return new ResponseEntity<>(new MessageResponse("Resource deleted successfully"), HttpStatus.OK);
     }
 }
+// Test
